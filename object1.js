820ac9ff7a6663ed42d1eae82778058c1bf34126
Dzulfanni <belajarayok866@gmail.com>
2020-09-11 06:53:01 +0700

    frist commit

---
+++ object1.js
@@ -0,0 +1,58 @@
+var motor ={
+    merk : 'ninja',
+    warna : 'hijau',
+    berat : 100,
+    rem : function(){
+        return "Motor Berhenti"
+    },
+    gas : function(){
+        return "Motor Standing"
+    }
+
+}
+
+
+var pesawat ={
+    merk : 'Garuda',
+    warna : 'Biru',
+    gas : function(){
+        return'take offf'
+    },
+rem : function(){
+        return 'landing'
+    }
+}
+
+var lampu = {
+    merk : 'philips',
+    konektor : 'kabel',
+    on : function(){
+        return 'lampuhidup'
+    },
+    off : function(){
+        return 'lampu mati'
+    }
+}
+
+var laptop = {
+    processor : 'AMD',
+    VGA : 'Invidia',
+    capsLock : function(){
+        return 'Huruf Kapital'
+},
+backSpace : function(){
+    return 'menghapus teks'
+}
+}
+
+var kipasAngin ={
+    warna : 'merah',
+    merk: 'miyako',
+    tinggi : 100,
+    on : function(){
+        return 'kipas menyala'
+    },
+    off : function(){
+        return 'kipasmati'
+    }
+}
